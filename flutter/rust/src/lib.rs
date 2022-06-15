6d50d84d9580f7a09d873a105a3296fbb2fa9bd5
Jörn Heinemann <joernheinemann@gmx.de>
2022-06-16 00:30:37 +0200

    added code from rust bridge

--- flutter/rust/src/lib.rs
+++ flutter/rust/src/lib.rs
@@ -1,8 +1,2 @@
-#[cfg(test)]
-mod tests {
-	#[test]
-	fn it_works() {
-		let result = 2 + 2;
-		assert_eq!(result, 4);
-	}
-}
+mod bridge_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
+mod sendclose;
---
+++ flutter/rust/src/sendclose.rs
@@ -0,0 +1,11 @@
+use sendclose_crypto::{aes, ecdh};
+
+pub fn aes_test()
+{
+	aes();
+}
+
+pub fn ed_test()
+{
+	ecdh();
+}
