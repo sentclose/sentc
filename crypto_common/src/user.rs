9213a3c8ddfc7e8c79f521749b875dd3da23a4ca
Jörn Heinemann <joernheinemann@gmx.de>
2022-06-30 20:04:03 +0200

    export common structs and enums to a new lib

---
+++ crypto_common/src/user.rs
@@ -0,0 +1,240 @@
+use serde::{Deserialize, Serialize};
+use serde_json::{from_slice, to_string};
+
+#[derive(Serialize, Deserialize)]
+pub struct MasterKey
+{
+	pub master_key_alg: String,
+	pub encrypted_master_key: String, //base64 encoded master key
+	pub encrypted_master_key_alg: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct KeyDerivedData
+{
+	pub derived_alg: String,
+	pub client_random_value: String, //don't use the enum for out, we will get the enum form the derived alg on the server (because the rand value is only used on the server)
+	pub hashed_authentication_key: String,
+
+	//pub/pri encrypt decrypt
+	pub public_key: String,
+	pub encrypted_private_key: String,
+	pub keypair_encrypt_alg: String,
+
+	//sign/verify
+	pub verify_key: String,
+	pub encrypted_sign_key: String,
+	pub keypair_sign_alg: String,
+}
+
+/**
+# Register Data for the server api
+
+send this after register to the server
+*/
+#[derive(Serialize, Deserialize)]
+pub struct RegisterData
+{
+	pub master_key: MasterKey,
+	pub derived: KeyDerivedData,
+}
+
+impl RegisterData
+{
+	pub fn from_string<F, E>(v: &[u8], err_fn: F) -> Result<Self, E>
+	where
+		F: Fn(serde_json::Error) -> E,
+		E: std::error::Error,
+	{
+		match from_slice::<Self>(v) {
+			Err(e) => Err(err_fn(e)),
+			Ok(v) => Ok(v),
+		}
+	}
+
+	pub fn to_string(&self) -> String
+	{
+		match to_string(self) {
+			Ok(v) => v,
+			Err(_e) => format!("{{\"status\": {}, \"error_message\": \"{}\"}}", -1, "Error while json parse"),
+		}
+	}
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ChangePasswordData
+{
+	pub new_derived_alg: String,
+	pub new_client_random_value: String,
+	pub new_hashed_authentication_key: String,
+	pub new_encrypted_master_key: String,
+	pub new_encrypted_master_key_alg: String,
+	pub old_auth_key: String,
+}
+
+impl ChangePasswordData
+{
+	pub fn from_string<F, E>(v: &[u8], err_fn: F) -> Result<Self, E>
+	where
+		F: Fn(serde_json::Error) -> E,
+		E: std::error::Error,
+	{
+		match from_slice::<Self>(v) {
+			Err(e) => Err(err_fn(e)),
+			Ok(v) => Ok(v),
+		}
+	}
+
+	pub fn to_string(&self) -> String
+	{
+		match to_string(self) {
+			Ok(v) => v,
+			Err(_e) => format!("{{\"status\": {}, \"error_message\": \"{}\"}}", -1, "Error while json parse"),
+		}
+	}
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum MasterKeyFormat
+{
+	Argon2(String),
+}
+
+impl MasterKeyFormat
+{
+	pub fn from_string(v: &[u8]) -> Result<Self, String>
+	{
+		match from_slice::<Self>(v) {
+			Err(_e) => Err(format!("{{\"status\": {}, \"error_message\": \"{}\"}}", -1, "Error while json parse")),
+			Ok(v) => Ok(v),
+		}
+	}
+
+	pub fn to_string(&self) -> String
+	{
+		match to_string(self) {
+			Ok(v) => v,
+			Err(_e) => format!("{{\"status\": {}, \"error_message\": \"{}\"}}", -1, "Error while json parse"),
+		}
+	}
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum PrivateKeyFormat
+{
+	Ecies(String),
+}
+
+impl PrivateKeyFormat
+{
+	pub fn from_string(v: &[u8]) -> serde_json::Result<Self>
+	{
+		//this function is used internally
+		from_slice::<Self>(v)
+	}
+
+	pub fn to_string(&self) -> String
+	{
+		match to_string(self) {
+			Ok(v) => v,
+			Err(_e) => format!("{{\"status\": {}, \"error_message\": \"{}\"}}", -1, "Error while json parse"),
+		}
+	}
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum PublicKeyFormat
+{
+	Ecies(String),
+}
+
+impl PublicKeyFormat
+{
+	pub fn from_string(v: &[u8]) -> serde_json::Result<Self>
+	{
+		//this function is used internally
+		from_slice::<Self>(v)
+	}
+
+	pub fn to_string(&self) -> String
+	{
+		match to_string(self) {
+			Ok(v) => v,
+			Err(_e) => format!("{{\"status\": {}, \"error_message\": \"{}\"}}", -1, "Error while json parse"),
+		}
+	}
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum SignKeyFormat
+{
+	Ed25519(String),
+}
+
+impl SignKeyFormat
+{
+	pub fn from_string(v: &[u8]) -> serde_json::Result<Self>
+	{
+		//this function is used internally
+		from_slice::<Self>(v)
+	}
+
+	pub fn to_string(&self) -> String
+	{
+		match to_string(self) {
+			Ok(v) => v,
+			Err(_e) => format!("{{\"status\": {}, \"error_message\": \"{}\"}}", -1, "Error while json parse"),
+		}
+	}
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum VerifyKeyFormat
+{
+	Ed25519(String),
+}
+
+impl VerifyKeyFormat
+{
+	pub fn from_string(v: &[u8]) -> serde_json::Result<Self>
+	{
+		//this function is used internally
+		from_slice::<Self>(v)
+	}
+
+	pub fn to_string(&self) -> String
+	{
+		match to_string(self) {
+			Ok(v) => v,
+			Err(_e) => format!("{{\"status\": {}, \"error_message\": \"{}\"}}", -1, "Error while json parse"),
+		}
+	}
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct KeyData
+{
+	pub private_key: PrivateKeyFormat,
+	pub public_key: PublicKeyFormat,
+	pub sign_key: SignKeyFormat,
+	pub verify_key: VerifyKeyFormat,
+}
+
+impl KeyData
+{
+	pub fn from_string(v: &[u8]) -> Result<Self, String>
+	{
+		match from_slice::<Self>(v) {
+			Err(_e) => Err(format!("{{\"status\": {}, \"error_message\": \"{}\"}}", -1, "Error while json parse")),
+			Ok(v) => Ok(v),
+		}
+	}
+
+	pub fn to_string(&self) -> String
+	{
+		match to_string(self) {
+			Ok(v) => v,
+			Err(_e) => format!("{{\"status\": {}, \"error_message\": \"{}\"}}", -1, "Error while json parse"),
+		}
+	}
+}
