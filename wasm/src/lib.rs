f5949665d2be7b5da8541fa87b0e8827d713be2e
Jörn Heinemann <joernheinemann@gmx.de>
2022-06-30 20:04:46 +0200

    update to the new public interface

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,4 +1,13 @@
-use sendclose_crypto::{aes, argon, ecdh, register as register_core, register_test, sign};
+use sendclose_crypto::{
+	aes,
+	argon,
+	done_login as done_login_core,
+	ecdh,
+	prepare_login as prepare_login_core,
+	register as register_core,
+	register_test,
+	sign,
+};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -36,4 +45,34 @@ pub fn register_test_full() -> String
 pub fn register(password: String) -> String
 {
 	register_core(password)
+}
+
+#[wasm_bindgen]
+pub fn prepare_login(password: String, salt_string: String, derived_encryption_key_alg: String) -> String
+{
+	prepare_login_core(password, salt_string, derived_encryption_key_alg)
+}
+
+#[wasm_bindgen]
+pub fn done_login(
+	master_key_encryption: String, //from the prepare login as base64 for exporting
+	encrypted_master_key: String,  //as base64 encoded string from the server
+	encrypted_private_key: String,
+	public_key_string: String,
+	keypair_encrypt_alg: String,
+	encrypted_sign_key: String,
+	verify_key_string: String,
+	keypair_sign_alg: String,
+) -> String
+{
+	done_login_core(
+		master_key_encryption,
+		encrypted_master_key,
+		encrypted_private_key,
+		public_key_string,
+		keypair_encrypt_alg,
+		encrypted_sign_key,
+		verify_key_string,
+		keypair_sign_alg,
+	)
 }
---
+++ wasm/tests/web/types/Enities.ts
@@ -0,0 +1,29 @@
+interface MasterKey
+{
+	master_key_alg: string,
+	encrypted_master_key: string, //base64 encoded master key
+	encrypted_master_key_alg: string,
+}
+
+interface KeyDerivedData
+{
+	derived_alg: string,
+	client_random_value: string,
+	hashed_authentication_key: string,
+
+	//pub/pri encrypt decrypt
+	public_key: string,
+	encrypted_private_key: string,
+	keypair_encrypt_alg: string,
+
+	//sign/verify
+	verify_key: string,
+	encrypted_sign_key: string,
+	keypair_sign_alg: string,
+}
+
+interface RegisterData
+{
+	master_key: MasterKey,
+	derived: KeyDerivedData,
+}
