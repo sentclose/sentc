915ea924d77175d5139af6dcba7926e6d1367b34
Jörn Heinemann <joernheinemann@gmx.de>
2022-07-01 13:46:05 +0200

    added register + login tests

--- crypto/src/user/user_rust.rs
+++ crypto/src/user/user_rust.rs
@@ -32,6 +32,10 @@ pub fn change_password(
 #[cfg(test)]
 mod test
 {
+	use base64ct::{Base64, Encoding};
+	use sendclose_crypto_common::user::{DoneLoginInput, RegisterData};
+	use sendclose_crypto_core::{ClientRandomValue, Sk};
+
 	use super::*;
 
 	#[test]
@@ -42,5 +46,61 @@ mod test
 		let out = register(password.to_string()).unwrap();
 
 		println!("rust: {}", out);
+	}
+
+	#[test]
+	fn test_register_and_login()
+	{
+		let password = "abc*èéöäüê";
+
+		let out = register(password.to_string()).unwrap();
+
+		let out = RegisterData::from_string(out.as_bytes()).unwrap();
+		let RegisterData {
+			derived,
+			master_key,
+		} = out;
+
+		//and now try to login
+		//normally the salt gets calc by the api
+		let client_random_value = Base64::decode_vec(derived.client_random_value.as_str()).unwrap();
+		let client_random_value = match derived.derived_alg.as_str() {
+			sendclose_crypto_core::ARGON_2_OUTPUT => ClientRandomValue::Argon2(client_random_value.try_into().unwrap()),
+			_ => panic!("alg not found"),
+		};
+
+		let salt_from_rand_value = sendclose_crypto_core::generate_salt(client_random_value);
+		let salt_from_rand_value = Base64::encode_string(&salt_from_rand_value);
+
+		//back to the client, send prep login out string to the server if it is no err
+		let (_, master_key_encryption_key) = prepare_login(password.to_string(), salt_from_rand_value, derived.derived_alg).unwrap();
+
+		//get the server output back
+		let server_output = DoneLoginInput {
+			encrypted_master_key: master_key.encrypted_master_key,
+			encrypted_private_key: derived.encrypted_private_key,
+			encrypted_sign_key: derived.encrypted_sign_key,
+			public_key_string: derived.public_key,
+			verify_key_string: derived.verify_key,
+			keypair_encrypt_alg: derived.keypair_encrypt_alg,
+			keypair_sign_alg: derived.keypair_sign_alg,
+		};
+
+		let server_output = server_output.to_string().unwrap();
+
+		//now save the values
+		let login_out = done_login(&master_key_encryption_key, server_output).unwrap();
+
+		let private_key = match login_out.private_key {
+			Sk::Ecies(k) => k,
+		};
+
+		let mut arr = [0u8; 32];
+		arr[0] = 123;
+		arr[1] = 255;
+		arr[2] = 254;
+		arr[3] = 0;
+
+		assert_ne!(private_key, arr);
 	}
 }
