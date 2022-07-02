d4ce2e0eb1d464051c61e90d2434e3caa2e10eb4
Jörn Heinemann <joernheinemann@gmx.de>
2022-07-02 13:15:24 +0200

    added done key rotation incl. test

--- crypto_core/src/group.rs
+++ crypto_core/src/group.rs
@@ -1,6 +1,7 @@
 use alloc::vec::Vec;
 
 use crate::alg::{asym, sym};
+use crate::crypto::{decrypt_asymmetric, decrypt_symmetric, encrypt_asymmetric};
 use crate::{Error, Pk, Sk, SymKey};
 
 pub struct CreateGroupOutput
@@ -22,6 +23,7 @@ pub struct KeyRotationOutput
 	pub public_group_key: Pk,
 	pub keypair_encrypt_alg: &'static str,
 	pub encrypted_group_key_by_ephemeral: Vec<u8>,
+	pub ephemeral_alg: &'static str,
 	pub encrypted_ephemeral_key: Vec<u8>, //encrypted by the old group key. encrypt this key with every other member public key on the server
 }
 
@@ -98,7 +100,7 @@ fn key_rotation_aes_ecies_ed25519(old_group_key: &SymKey, invoker_public_key: &P
 	//4. encrypt the new group with the ephemeral_key.
 	let encrypted_group_key_by_ephemeral = sym::aes_gcm::encrypt_with_generated_key(raw_ephemeral_key, raw_group_key)?;
 
-	//5. encrypt the ephemeral key with the old group key, so all group member can get the new key
+	//5. encrypt the ephemeral key with the old group key, so all group member can get the new key. this encrypted ephemeral key will get encrypted by every group uses public key
 	let encrypted_ephemeral_key = match old_group_key {
 		SymKey::Aes(k) => sym::aes_gcm::encrypt_with_generated_key(k, raw_ephemeral_key)?,
 	};
@@ -121,6 +123,7 @@ fn key_rotation_aes_ecies_ed25519(old_group_key: &SymKey, invoker_public_key: &P
 		encrypted_group_key_by_ephemeral,
 		public_group_key: keypair.pk,
 		encrypted_ephemeral_key,
+		ephemeral_alg: sym::aes_gcm::AES_GCM_OUTPUT,
 	})
 }
 
@@ -128,6 +131,41 @@ pub fn key_rotation(old_group_key: &SymKey, invoker_public_key: &Pk) -> Result<K
 {
 	#[cfg(feature = "argon2_aes_ecies_ed25519")]
 	key_rotation_aes_ecies_ed25519(old_group_key, invoker_public_key)
+}
+
+pub fn done_key_rotation(
+	private_key: &Sk,
+	public_key: &Pk,
+	old_group: &SymKey,
+	encrypted_ephemeral_key_by_group_key_and_public_key: &[u8],
+	encrypted_group_key_by_ephemeral: &[u8],
+	ephemeral_alg: &str,
+) -> Result<Vec<u8>, Error>
+{
+	//1. decrypt the encrypted ephemeral key with the private key
+	let decrypted_encrypted_ephemeral_key = decrypt_asymmetric(private_key, encrypted_ephemeral_key_by_group_key_and_public_key)?;
+
+	//2. decrypt the encrypted ephemeral key then with the old group key (the previous group key)
+	let decrypted_ephemeral_key = decrypt_symmetric(old_group, &decrypted_encrypted_ephemeral_key)?;
+
+	//3.decrypt the new group key with the decrypted ephemeral key
+	let new_group_key = match ephemeral_alg {
+		sym::aes_gcm::AES_GCM_OUTPUT => {
+			//decrypt directly here with the key because we know which key was used thanks to the ephemeral_alg
+			let key = &decrypted_ephemeral_key
+				.try_into()
+				.map_err(|_| Error::KeyDecryptFailed)?;
+
+			sym::aes_gcm::decrypt_with_generated_key(key, encrypted_group_key_by_ephemeral)?
+		},
+		_ => return Err(Error::AlgNotFound),
+	};
+
+	//4. encrypt the new group key with the public key
+	let encrypted_new_group_key = encrypt_asymmetric(public_key, &new_group_key)?;
+
+	//the user can call ger group to get the new pri key too
+	Ok(encrypted_new_group_key)
 }
 
 pub fn get_group(
@@ -139,9 +177,7 @@ pub fn get_group(
 ) -> Result<(SymKey, Sk), Error>
 {
 	//1. decrypt the group key
-	let decrypted_group_key = match private_key {
-		Sk::Ecies(_) => asym::ecies::decrypt(private_key, encrypted_group_key)?,
-	};
+	let decrypted_group_key = decrypt_asymmetric(private_key, encrypted_group_key)?;
 
 	//turn the decrypted group key into a Sym key
 	let (decrypted_group_key, decrypted_private_group_key) = match group_key_alg {
@@ -183,6 +219,7 @@ mod test
 
 	use super::*;
 	use crate::alg::sym::aes_gcm::AES_GCM_OUTPUT;
+	use crate::crypto::{decrypt_asymmetric, decrypt_symmetric, encrypt_asymmetric, encrypt_symmetric};
 	use crate::generate_salt;
 	use crate::user::{done_login, prepare_login, register, LoginDoneOutput};
 
@@ -234,27 +271,116 @@ mod test
 		.unwrap();
 
 		//test encrypt / decrypt
+		let text = "abc 12345 üöä*#+^°êéè";
+
+		let encrypted = encrypt_symmetric(&group_key, text.as_bytes()).unwrap();
+		let decrypted = decrypt_symmetric(&group_key, &encrypted).unwrap();
+
+		assert_eq!(decrypted, text.as_bytes());
+
+		let decrypted_text = std::str::from_utf8(&decrypted).unwrap();
+		assert_eq!(decrypted_text, text);
+
+		let encrypted_pri = encrypt_asymmetric(&group_out.public_group_key, text.as_bytes()).unwrap();
+		let decrypted_pri = decrypt_asymmetric(&group_pri_key, &encrypted_pri).unwrap();
+
+		assert_eq!(decrypted_pri, text.as_bytes());
+
+		let decrypted_text = std::str::from_utf8(&decrypted_pri).unwrap();
+		assert_eq!(decrypted_text, text);
+	}
+
+	#[test]
+	fn test_key_rotation()
+	{
+		let (pk, login_out) = create_dummy_user();
+
+		let group_out = prepare_create(&pk).unwrap();
+
+		#[cfg(feature = "argon2_aes_ecies_ed25519")]
+		assert_eq!(group_out.group_key_alg, AES_GCM_OUTPUT);
+
+		//decrypt the group key
+		let (group_key, _group_pri_key) = get_group(
+			&login_out.private_key,
+			&group_out.encrypted_group_key,
+			&group_out.encrypted_private_group_key,
+			group_out.group_key_alg,
+			group_out.keypair_encrypt_alg,
+		)
+		.unwrap();
+
+		let rotation_out = key_rotation(&group_key, &pk).unwrap();
+
+		//it should get the values from own encrypted group key
+		let (new_group_key, _new_group_pri_key) = get_group(
+			&login_out.private_key,
+			&rotation_out.encrypted_group_key_by_user,
+			&rotation_out.encrypted_private_group_key,
+			rotation_out.group_key_alg,
+			rotation_out.keypair_encrypt_alg,
+		)
+		.unwrap();
+
 		#[cfg(feature = "argon2_aes_ecies_ed25519")]
 		{
-			let text = "abc 12345 üöä*#+^°êéè";
+			let old_key = match &group_key {
+				SymKey::Aes(k) => k,
+			};
 
-			let encrypted = sym::aes_gcm::encrypt(&group_key, text.as_bytes()).unwrap();
+			let new_key = match &new_group_key {
+				SymKey::Aes(k) => k,
+			};
 
-			let decrypted = sym::aes_gcm::decrypt(&group_key, &encrypted).unwrap();
+			//should not the same because this is a new group key
+			assert_ne!(old_key, new_key);
+		}
 
-			assert_eq!(decrypted, text.as_bytes());
+		//do the server key rotation
+		//executed on the server not the client. the client invokes done_key_rotation after
+		//1. encrypt the encrypted ephemeral key with the public key
+		//2. save this key in the db
+		let encrypted_ephemeral_key_by_group_key_and_public_key = encrypt_asymmetric(&pk, &rotation_out.encrypted_ephemeral_key).unwrap();
 
-			let decrypted_text = std::str::from_utf8(&decrypted).unwrap();
-			assert_eq!(decrypted_text, text);
+		//the encrypted_group_key_by_ephemeral is for everyone the same because this is encrypted by the previous group key
 
-			let encrypted_pri = asym::ecies::encrypt(&group_out.public_group_key, text.as_bytes()).unwrap();
+		//done key rotation to get the new group key
+		let out = done_key_rotation(
+			&login_out.private_key,
+			&pk,
+			&group_key,
+			&encrypted_ephemeral_key_by_group_key_and_public_key,
+			&rotation_out.encrypted_group_key_by_ephemeral,
+			rotation_out.ephemeral_alg,
+		)
+		.unwrap();
 
-			let decrypted_pri = asym::ecies::decrypt(&group_pri_key, &encrypted_pri).unwrap();
+		//get the new group by get_group
+		let (new_group_key2, _new_group_pri_key2) = get_group(
+			&login_out.private_key,
+			&out,
+			&rotation_out.encrypted_private_group_key,
+			rotation_out.group_key_alg,
+			rotation_out.keypair_encrypt_alg,
+		)
+		.unwrap();
 
-			assert_eq!(decrypted_pri, text.as_bytes());
+		#[cfg(feature = "argon2_aes_ecies_ed25519")]
+		{
+			let old_key = match &group_key {
+				SymKey::Aes(k) => k,
+			};
 
-			let decrypted_text = std::str::from_utf8(&decrypted_pri).unwrap();
-			assert_eq!(decrypted_text, text);
+			let new_key = match &new_group_key {
+				SymKey::Aes(k) => k,
+			};
+
+			let new_key2 = match &new_group_key2 {
+				SymKey::Aes(k) => k,
+			};
+
+			assert_eq!(new_key, new_key2); //should be the same
+			assert_ne!(old_key, new_key2); //should not the same because this is a new group key
 		}
 	}
 }
