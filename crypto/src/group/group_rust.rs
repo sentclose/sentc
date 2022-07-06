1d911d02f8cbc159ddc3d9b6b73ad5fe7241ada2
Jörn Heinemann <joernheinemann@gmx.de>
2022-07-06 21:12:27 +0200

    added feature macro for every function because of ide err

--- crypto/src/group/group_rust.rs
+++ crypto/src/group/group_rust.rs
@@ -20,16 +20,21 @@ pub struct GroupOutData
 	pub group_id: String,
 }
 
+#[cfg(feature = "rust")]
 pub fn prepare_create(creators_public_key: &PublicKeyFormat) -> Result<String, Error>
 {
+	#[cfg(feature = "rust")]
 	prepare_create_internally(&creators_public_key)
 }
 
+#[cfg(feature = "rust")]
 pub fn key_rotation(previous_group_key: &SymKeyFormat, invoker_public_key: &PublicKeyFormat) -> Result<String, Error>
 {
+	#[cfg(feature = "rust")]
 	key_rotation_internally(&previous_group_key, &invoker_public_key)
 }
 
+#[cfg(feature = "rust")]
 pub fn done_key_rotation(
 	private_key: &PrivateKeyFormat,
 	public_key: &PublicKeyFormat,
@@ -37,14 +42,18 @@ pub fn done_key_rotation(
 	server_output: &KeyRotationInput,
 ) -> Result<String, Error>
 {
+	#[cfg(feature = "rust")]
 	done_key_rotation_internally(&private_key, &public_key, &previous_group_key, server_output)
 }
 
+#[cfg(feature = "rust")]
 fn get_group_keys(private_key: &PrivateKeyFormatInt, server_output: &GroupKeyServerOutput) -> Result<GroupKeyData, Error>
 {
+	#[cfg(feature = "rust")]
 	get_group_keys_internally(private_key, server_output)
 }
 
+#[cfg(feature = "rust")]
 pub fn get_group_data(private_key: &PrivateKeyFormat, server_output: &GroupServerData) -> Result<GroupOutData, Error>
 {
 	let mut keys = Vec::with_capacity(server_output.keys.len());
@@ -59,7 +68,9 @@ pub fn get_group_data(private_key: &PrivateKeyFormat, server_output: &GroupServe
 	})
 }
 
+#[cfg(feature = "rust")]
 pub fn prepare_group_keys_for_new_member(requester_public_key: &PublicKeyFormat, group_keys: &[&SymKeyFormat]) -> Result<String, Error>
 {
+	#[cfg(feature = "rust")]
 	prepare_group_keys_for_new_member_internally(&requester_public_key, group_keys)
 }
