5ab3c4aee5233563e1f342eb4c07db06d8ab32e7
Jörn Heinemann <joernheinemann@gmx.de>
2022-07-11 21:50:56 +0200

    added crypto fn to wasm

--- implementation/js/sentc_wasm/src/crypto.rs
+++ implementation/js/sentc_wasm/src/crypto.rs
@@ -26,12 +26,83 @@ impl CryptoRawOutput
 }
 
 #[wasm_bindgen]
-pub fn encrypt_raw_symmetric(key: String, data: &[u8]) -> Result<CryptoRawOutput, String>
+pub fn encrypt_raw_symmetric(key: String, data: &[u8], sign_key: &str) -> Result<CryptoRawOutput, String>
 {
-	let (head, data) = crypto::encrypt_raw_symmetric(key.as_str(), data, "")?;
+	let (head, data) = crypto::encrypt_raw_symmetric(key.as_str(), data, sign_key)?;
 
 	Ok(CryptoRawOutput {
 		head,
 		data,
 	})
+}
+
+#[wasm_bindgen]
+pub fn decrypt_raw_symmetric(key: &str, encrypted_data: &[u8], head: &str, verify_key_data: &str) -> Result<Vec<u8>, String>
+{
+	crypto::decrypt_raw_symmetric(key, encrypted_data, head, verify_key_data)
+}
+
+#[wasm_bindgen]
+pub fn encrypt_symmetric(key: &str, data: &[u8], sign_key: &str) -> Result<Vec<u8>, String>
+{
+	crypto::encrypt_symmetric(key, data, sign_key)
+}
+
+#[wasm_bindgen]
+pub fn decrypt_symmetric(key: &str, encrypted_data: &[u8], verify_key_data: &str) -> Result<Vec<u8>, String>
+{
+	crypto::decrypt_symmetric(key, encrypted_data, verify_key_data)
+}
+
+#[wasm_bindgen]
+pub fn encrypt_string_symmetric(key: &str, data: &[u8], sign_key: &str) -> Result<String, String>
+{
+	crypto::encrypt_string_symmetric(key, data, sign_key)
+}
+
+#[wasm_bindgen]
+pub fn decrypt_string_symmetric(key: &str, encrypted_data: &str, verify_key_data: &str) -> Result<Vec<u8>, String>
+{
+	crypto::decrypt_string_symmetric(key, encrypted_data, verify_key_data)
+}
+
+#[wasm_bindgen]
+pub fn encrypt_raw_asymmetric(reply_public_key_data: &str, data: &[u8], sign_key: &str) -> Result<CryptoRawOutput, String>
+{
+	let (head, data) = crypto::encrypt_raw_asymmetric(reply_public_key_data, data, sign_key)?;
+
+	Ok(CryptoRawOutput {
+		head,
+		data,
+	})
+}
+
+#[wasm_bindgen]
+pub fn decrypt_raw_asymmetric(private_key: &str, encrypted_data: &[u8], head: &str, verify_key_data: &str) -> Result<Vec<u8>, String>
+{
+	crypto::decrypt_raw_asymmetric(private_key, encrypted_data, head, verify_key_data)
+}
+
+#[wasm_bindgen]
+pub fn encrypt_asymmetric(reply_public_key_data: &str, data: &[u8], sign_key: &str) -> Result<Vec<u8>, String>
+{
+	crypto::encrypt_asymmetric(reply_public_key_data, data, sign_key)
+}
+
+#[wasm_bindgen]
+pub fn decrypt_asymmetric(private_key: &str, encrypted_data: &[u8], verify_key_data: &str) -> Result<Vec<u8>, String>
+{
+	crypto::decrypt_asymmetric(private_key, encrypted_data, verify_key_data)
+}
+
+#[wasm_bindgen]
+pub fn encrypt_string_asymmetric(reply_public_key_data: &str, data: &[u8], sign_key: &str) -> Result<String, String>
+{
+	crypto::encrypt_string_asymmetric(reply_public_key_data, data, sign_key)
+}
+
+#[wasm_bindgen]
+pub fn decrypt_string_asymmetric(private_key: &str, encrypted_data: &str, verify_key_data: &str) -> Result<Vec<u8>, String>
+{
+	crypto::decrypt_string_asymmetric(private_key, encrypted_data, verify_key_data)
 }
