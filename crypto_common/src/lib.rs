30e4ae7fc62cb59165f151c11c6c010a2b08cc88
Jörn Heinemann <joernheinemann@gmx.de>
2022-07-13 11:58:55 +0200

    added general id types

--- crypto_common/src/lib.rs
+++ crypto_common/src/lib.rs
@@ -2,6 +2,15 @@
 
 extern crate alloc;
 
+use alloc::string::String;
+
 pub mod crypto;
 pub mod group;
 pub mod user;
+
+type GeneralIdFormat = String;
+pub type GroupId = GeneralIdFormat;
+pub type UserId = GeneralIdFormat;
+pub type EncryptionKeyPairId = GeneralIdFormat;
+pub type SignKeyPairId = GeneralIdFormat;
+pub type SymKeyId = GeneralIdFormat;
