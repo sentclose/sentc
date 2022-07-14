a7bb16af37c3ac667019e17ef66e4a3de3053f0f
Jörn Heinemann <joernheinemann@gmx.de>
2022-07-14 22:58:49 +0200

    removed unused fn

--- crypto/src/util/util_non_rust.rs
+++ crypto/src/util/util_non_rust.rs
@@ -111,19 +111,6 @@ pub struct KeyData
 	pub jwt: String,
 }
 
-impl KeyData
-{
-	pub fn from_string(v: &str) -> serde_json::Result<Self>
-	{
-		from_str::<Self>(v)
-	}
-
-	pub fn to_string(&self) -> serde_json::Result<String>
-	{
-		to_string(self)
-	}
-}
-
 #[derive(Serialize, Deserialize)]
 pub enum SymKeyFormat
 {
