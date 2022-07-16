a6518be3b4724b184aece5ccd1f520103c1e93c6
Jörn Heinemann <joernheinemann@gmx.de>
2022-07-16 12:21:14 +0200

    moved files into extra dir

--- implementation/js/sentc_wasm/tests/web_test/web_as_module/js.js
+++ implementation/js/sentc_wasm/tests/web_test/web_as_module/js.js
@@ -14,7 +14,7 @@ import init, {
 	decrypt_string_symmetric,
 	generate_non_register_sym_key,
 	decrypt_sym_key
-} from './../../pkg/sentc_wasm.js';
+} from "./../../../pkg/sentc_wasm.js";
 
 export async function run()
 {
@@ -23,35 +23,35 @@ export async function run()
 	console.log("_________________________________");
 
 	console.log("register test");
-	let string5 = register_test_full();
+	const string5 = register_test_full();
 
 	console.log(string5);
 
 	console.log("_________________________________");
 	console.log("_________________________________");
-	console.log("real usage")
+	console.log("real usage");
 
-	let pw = "hello";
+	const pw = "hello";
 
-	console.log("register user")
-	let register_out = register_test("admin",pw);
+	console.log("register user");
+	const register_out = register_test("admin", pw);
 
 	console.log(register_out);
 	console.log("real 1 json pretty");
 
 	console.log("_________________________________");
-	console.log("login")
-	console.log("prepare login")
-	let prep_server_out = simulate_server_prepare_login(register_out)
+	console.log("login");
+	console.log("prepare login");
+	const prep_server_out = simulate_server_prepare_login(register_out);
 
-	let prep = prepare_login_test(pw,prep_server_out);
+	const prep = prepare_login_test(pw, prep_server_out);
 
-	let done_login_server_out = simulate_server_done_login(register_out);
+	const done_login_server_out = simulate_server_done_login(register_out);
 
-	let key_data = done_login_test(prep.get_master_key_encryption_key(),done_login_server_out);
+	const key_data = done_login_test(prep.get_master_key_encryption_key(), done_login_server_out);
 
 	/** @param {KeyData} */
-	let keys = {
+	const keys = {
 		private_key: key_data.get_private_key(),
 		public_key: key_data.get_public_key(),
 		sign_key: key_data.get_sign_key(),
@@ -60,7 +60,7 @@ export async function run()
 		exported_verify_key: key_data.get_exported_verify_key()
 	};
 
-	let jwt = key_data.get_jwt();
+	const jwt = key_data.get_jwt();
 
 	console.log(keys);
 	console.log(jwt);
@@ -68,69 +68,69 @@ export async function run()
 	console.log("_________________________________");
 	console.log("create group");
 
-	let group_create_out = prepare_create(keys.public_key);
+	const group_create_out = prepare_create(keys.public_key);
 	console.log(group_create_out);
 
 	console.log("get group");
-	let group_server_out = simulate_server_create_group(group_create_out);
+	const group_server_out = simulate_server_create_group(group_create_out);
 
 	console.log(group_server_out);
 
-	let group_data = get_group_data(keys.private_key,group_server_out);
+	const group_data = get_group_data(keys.private_key, group_server_out);
 
 	console.log(group_data);
 
 	/** @param {GroupData} */
-	let group_keys = JSON.parse(group_data);
+	const group_keys = JSON.parse(group_data);
 
 	console.log(group_keys);
 
 	console.log(group_keys.keys[0].group_key);
 
 	console.log("sym encrypt test");
-	let text = "abc";
-	let text_view = stringToByteArray(text);
+	const text = "abc";
+	const text_view = stringToByteArray(text);
 
-	let encrypted = encrypt_symmetric(group_keys.keys[0].group_key, text_view, keys.sign_key);
+	const encrypted = encrypt_symmetric(group_keys.keys[0].group_key, text_view, keys.sign_key);
 
 	console.log(encrypted);
 
-	let decrypted = decrypt_symmetric(group_keys.keys[0].group_key,encrypted,keys.exported_verify_key);
+	const decrypted = decrypt_symmetric(group_keys.keys[0].group_key, encrypted, keys.exported_verify_key);
 
 	console.log(byteArrayToString(decrypted));
 
 	console.log("sym encrypt test with string");
 
-	let encrypted_string = encrypt_string_symmetric(group_keys.keys[0].group_key, text_view, keys.sign_key);
+	const encrypted_string = encrypt_string_symmetric(group_keys.keys[0].group_key, text_view, keys.sign_key);
 
 	console.log(encrypted_string);
 
-	let decrypted_string = decrypt_string_symmetric(group_keys.keys[0].group_key,encrypted_string,keys.exported_verify_key);
+	const decrypted_string = decrypt_string_symmetric(group_keys.keys[0].group_key, encrypted_string, keys.exported_verify_key);
 
 	console.log(byteArrayToString(decrypted_string));
 
 	console.log("_________________________________");
-	console.log("key generate")
+	console.log("key generate");
 
-	let generated_key_out = generate_non_register_sym_key(group_keys.keys[0].group_key);
-	let generated_key = generated_key_out.get_key();
-	let encrypted_generated_key = generated_key_out.get_encrypted_key();
+	const generated_key_out = generate_non_register_sym_key(group_keys.keys[0].group_key);
+	const generated_key = generated_key_out.get_key();
+	const encrypted_generated_key = generated_key_out.get_encrypted_key();
 
-	let encrypted_string1 = encrypt_string_symmetric(generated_key, text_view, keys.sign_key);
+	const encrypted_string1 = encrypt_string_symmetric(generated_key, text_view, keys.sign_key);
 
 	console.log(encrypted_string1);
 
-	let decrypted_string1 = decrypt_string_symmetric(generated_key,encrypted_string1,keys.exported_verify_key);
+	const decrypted_string1 = decrypt_string_symmetric(generated_key, encrypted_string1, keys.exported_verify_key);
 
 	console.log(byteArrayToString(decrypted_string1));
 
-	let decrypted_generated_key = decrypt_sym_key(group_keys.keys[0].group_key,encrypted_generated_key);
+	const decrypted_generated_key = decrypt_sym_key(group_keys.keys[0].group_key, encrypted_generated_key);
 
-	let encrypted_string2 = encrypt_string_symmetric(decrypted_generated_key, text_view, keys.sign_key);
+	const encrypted_string2 = encrypt_string_symmetric(decrypted_generated_key, text_view, keys.sign_key);
 
 	console.log(encrypted_string2);
 
-	let decrypted_string2 = decrypt_string_symmetric(decrypted_generated_key,encrypted_string2,keys.exported_verify_key);
+	const decrypted_string2 = decrypt_string_symmetric(decrypted_generated_key, encrypted_string2, keys.exported_verify_key);
 
 	console.log(byteArrayToString(decrypted_string2));
 }
