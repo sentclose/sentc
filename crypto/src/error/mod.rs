d29c56d935b1737789bfedeeff5ebb6f352a9dfc
Jörn Heinemann <joernheinemann@gmx.de>
2022-07-29 19:15:42 +0200

    changed error code between server and client error

--- crypto/src/error/mod.rs
+++ crypto/src/error/mod.rs
@@ -1,5 +1,5 @@
 use alloc::format;
-use alloc::string::String;
+use alloc::string::{String, ToString};
 
 use sentc_crypto_core::Error;
 
@@ -56,25 +56,30 @@ pub fn err_to_msg(error: SdkError) -> String
 	match error {
 		SdkError::Base(base_error) => {
 			match base_error {
-				Error::AlgNotFound => out_error(1, "The algorithms for this action was not found."),
+				Error::AlgNotFound => out_error("client_1", "The algorithms for this action was not found."),
 
-				Error::DecodePrivateKeyFailed => out_error(3, "The private key has a wrong format."),
-				_ => out_error(0, "other"),
+				Error::DecodePrivateKeyFailed => out_error("client_3", "The private key has a wrong format."),
+				_ => out_error("client_0", "other"),
 			}
 		},
-		SdkError::AlgNotFound => out_error(1, "The algorithms for this action was not found."),
-		SdkError::JsonToStringFailed => out_error(100, "Cannot create a string from this object"),
-		SdkError::JsonParseFailed => out_error(101, "Cannot create an object from the input string"),
+		SdkError::AlgNotFound => out_error("client_1", "The algorithms for this action was not found."),
+		SdkError::JsonToStringFailed => out_error("client_100", "Cannot create a string from this object"),
+		SdkError::JsonParseFailed => out_error("client_101", "Cannot create an object from the input string"),
 		//key decode error (from base64 string to the enum
-		SdkError::DerivedKeyWrongFormat => out_error(2, "The encrypted key has a wrong format."),
+		SdkError::DerivedKeyWrongFormat => out_error("client_2", "The encrypted key has a wrong format."),
 		//salt decode error (from base64 string to bytes)
-		SdkError::DecodeSaltFailed => out_error(4, "The salt has a wrong format"),
-		SdkError::ServerErr(code, msg) => out_error(code, msg.as_str()),
-		_ => out_error(0, "other"),
+		SdkError::DecodeSaltFailed => out_error("client_4", "The salt has a wrong format"),
+		SdkError::ServerErr(code, msg) => {
+			out_error(
+				(String::from("server_") + code.to_string().as_str()).as_str(),
+				msg.as_str(),
+			)
+		},
+		_ => out_error("client_0", "other"),
 	}
 }
 
-pub(crate) fn out_error(code: u32, message: &str) -> String
+pub(crate) fn out_error(code: &str, message: &str) -> String
 {
 	//create the error in json to communicate with the other implementations, so they can use their own error handling
 
