ec940a4c18729ae161e598c4780031ba86377cbb
Jörn Heinemann <joernheinemann@gmx.de>
2022-07-31 21:34:10 +0200

    added fn to get the alg as str to a given key

--- crypto_core/src/alg/asym/mod.rs
+++ crypto_core/src/alg/asym/mod.rs
@@ -1,3 +1,5 @@
+use crate::ECIES_OUTPUT;
+
 pub(crate) mod ecies;
 
 pub enum Pk
@@ -15,4 +17,18 @@ pub(crate) struct AsymKeyOutput
 	pub pk: Pk,
 	pub sk: Sk,
 	pub alg: &'static str,
+}
+
+pub fn getting_alg_from_private_key(key: &Sk) -> &'static str
+{
+	match key {
+		Sk::Ecies(_) => ECIES_OUTPUT,
+	}
+}
+
+pub fn getting_alg_from_public_key(key: &Pk) -> &'static str
+{
+	match key {
+		Pk::Ecies(_) => ECIES_OUTPUT,
+	}
 }
--- crypto_core/src/alg/sign/mod.rs
+++ crypto_core/src/alg/sign/mod.rs
@@ -1,3 +1,5 @@
+use crate::ED25519_OUTPUT;
+
 pub(crate) mod ed25519;
 
 pub enum SignK
@@ -15,4 +17,18 @@ pub(crate) struct SignOutput
 	pub alg: &'static str,
 	pub sign_key: SignK,
 	pub verify_key: VerifyK,
+}
+
+pub fn get_alg_from_sign_key(key: SignK) -> &'static str
+{
+	match key {
+		SignK::Ed25519(_) => ED25519_OUTPUT,
+	}
+}
+
+pub fn get_alg_from_verify_key(key: VerifyK) -> &'static str
+{
+	match key {
+		VerifyK::Ed25519(_) => ED25519_OUTPUT,
+	}
 }
