ad6751f92f57fec8af7dbb5f05f86d633872de76
Jörn Heinemann <joernheinemann@gmx.de>
2022-08-01 10:55:41 +0200

    added feature to export every sdk common from the sdk to avoid version conflict

--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -16,9 +16,9 @@ pub mod util_server;
 /**
 For server testing export every common
 because using common from path via submodule resolve in version conflicts when using it in tests
-*/
+ */
 #[cfg(feature = "server_test")]
-pub use sentc_crypto_common::*;
+pub use sentc_crypto_common as sdk_common;
 
 pub use self::error::{err_to_msg, SdkError};
 pub use self::util::{KeyData, PrivateKeyFormat, PublicKeyFormat, SignKeyFormat, SymKeyFormat, VerifyKeyFormat};
