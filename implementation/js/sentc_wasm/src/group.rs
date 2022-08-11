1b78adff7dd5d9e5b9e82adf3b78e88926740544
Jörn Heinemann <joernheinemann@gmx.de>
2022-08-11 16:39:50 +0200

    added invite, join req and key rotation wasm fn

--- implementation/js/sentc_wasm/src/group.rs
+++ implementation/js/sentc_wasm/src/group.rs
@@ -236,3 +236,325 @@ pub async fn get_group_keys(
 }
 
 //__________________________________________________________________________________________________
+//invite
+
+/**
+Prepare all group keys for a new member.
+
+Use the group keys from get group data or get group keys fn as string array
+*/
+#[wasm_bindgen]
+pub fn prepare_keys_for_new_member(user_public_key: &str, group_keys: &str, key_count: i32, admin_rank: i32) -> Result<String, JsValue>
+{
+	group::check_make_invite_req(admin_rank)?;
+
+	let key_session = if key_count > 50 { true } else { false };
+
+	let input = group::prepare_group_keys_for_new_member(user_public_key, group_keys, key_session)?;
+
+	Ok(input)
+}
+
+#[wasm_bindgen]
+pub async fn invite_user(
+	base_url: String,
+	auth_token: String,
+	jwt: String,
+	id: String,
+	user_id: String,
+	key_count: i32,
+	admin_rank: i32,
+	user_public_key: String,
+	group_keys: String,
+) -> Result<String, JsValue>
+{
+	let out = sentc_crypto_full::group::invite_user(
+		base_url,
+		auth_token.as_str(),
+		jwt.as_str(),
+		id.as_str(),
+		user_id.as_str(),
+		key_count,
+		admin_rank,
+		user_public_key.as_str(),
+		group_keys.as_str(),
+	)
+	.await?;
+
+	match out {
+		Some(id) => Ok(id),
+		None => Ok(String::from("")),
+	}
+}
+
+#[wasm_bindgen]
+pub async fn invite_user_session(
+	base_url: String,
+	auth_token: String,
+	jwt: String,
+	id: String,
+	session_id: String,
+	user_public_key: String,
+	group_keys: String,
+) -> Result<(), JsValue>
+{
+	sentc_crypto_full::group::invite_user_session(
+		base_url,
+		auth_token.as_str(),
+		jwt.as_str(),
+		id.as_str(),
+		session_id.as_str(),
+		user_public_key.as_str(),
+		group_keys.as_str(),
+	)
+	.await?;
+
+	Ok(())
+}
+
+#[wasm_bindgen]
+pub async fn get_invites_for_user(
+	base_url: String,
+	auth_token: String,
+	jwt: String,
+	last_fetched_time: String,
+	last_fetched_group_id: String,
+) -> Result<JsValue, JsValue>
+{
+	let out = sentc_crypto_full::group::get_invites_for_user(
+		base_url,
+		auth_token.as_str(),
+		jwt.as_str(),
+		last_fetched_time.as_str(),
+		last_fetched_group_id.as_str(),
+	)
+	.await?;
+
+	Ok(JsValue::from_serde(&out).unwrap())
+}
+
+#[wasm_bindgen]
+pub async fn accept_invite(base_url: String, auth_token: String, jwt: String, id: String) -> Result<(), JsValue>
+{
+	sentc_crypto_full::group::accept_invite(base_url, auth_token.as_str(), jwt.as_str(), id.as_str()).await?;
+
+	Ok(())
+}
+
+#[wasm_bindgen]
+pub async fn reject_invite(base_url: String, auth_token: String, jwt: String, id: String) -> Result<(), JsValue>
+{
+	sentc_crypto_full::group::reject_invite(base_url, auth_token.as_str(), jwt.as_str(), id.as_str()).await?;
+
+	Ok(())
+}
+
+//__________________________________________________________________________________________________
+//join req
+
+#[wasm_bindgen]
+pub async fn join_req(base_url: String, auth_token: String, jwt: String, id: String) -> Result<(), JsValue>
+{
+	sentc_crypto_full::group::join_req(base_url, auth_token.as_str(), jwt.as_str(), id.as_str()).await?;
+
+	Ok(())
+}
+
+#[wasm_bindgen]
+pub async fn get_join_reqs(
+	base_url: String,
+	auth_token: String,
+	jwt: String,
+	id: String,
+	admin_rank: i32,
+	last_fetched_time: String,
+	last_fetched_id: String,
+) -> Result<JsValue, JsValue>
+{
+	let out = sentc_crypto_full::group::get_join_reqs(
+		base_url,
+		auth_token.as_str(),
+		jwt.as_str(),
+		id.as_str(),
+		admin_rank,
+		last_fetched_time.as_str(),
+		last_fetched_id.as_str(),
+	)
+	.await?;
+
+	Ok(JsValue::from_serde(&out).unwrap())
+}
+
+#[wasm_bindgen]
+pub async fn reject_join_req(
+	base_url: String,
+	auth_token: String,
+	jwt: String,
+	id: String,
+	admin_rank: i32,
+	rejected_user_id: String,
+) -> Result<(), JsValue>
+{
+	sentc_crypto_full::group::reject_join_req(
+		base_url,
+		auth_token.as_str(),
+		jwt.as_str(),
+		id.as_str(),
+		admin_rank,
+		rejected_user_id.as_str(),
+	)
+	.await?;
+
+	Ok(())
+}
+
+#[wasm_bindgen]
+pub async fn accept_join_req(
+	base_url: String,
+	auth_token: String,
+	jwt: String,
+	id: String,
+	user_id: String,
+	key_count: i32,
+	admin_rank: i32,
+	user_public_key: String,
+	group_keys: String,
+) -> Result<String, JsValue>
+{
+	let out = sentc_crypto_full::group::accept_join_req(
+		base_url,
+		auth_token.as_str(),
+		jwt.as_str(),
+		id.as_str(),
+		user_id.as_str(),
+		key_count,
+		admin_rank,
+		user_public_key.as_str(),
+		group_keys.as_str(),
+	)
+	.await?;
+
+	match out {
+		Some(id) => Ok(id),
+		None => Ok(String::from("")),
+	}
+}
+
+#[wasm_bindgen]
+pub async fn join_user_session(
+	base_url: String,
+	auth_token: String,
+	jwt: String,
+	id: String,
+	session_id: String,
+	user_public_key: String,
+	group_keys: String,
+) -> Result<(), JsValue>
+{
+	sentc_crypto_full::group::join_user_session(
+		base_url,
+		auth_token.as_str(),
+		jwt.as_str(),
+		id.as_str(),
+		session_id.as_str(),
+		user_public_key.as_str(),
+		group_keys.as_str(),
+	)
+	.await?;
+
+	Ok(())
+}
+
+//__________________________________________________________________________________________________
+
+#[wasm_bindgen]
+pub async fn leave_group(base_url: String, auth_token: String, jwt: String, id: String) -> Result<(), JsValue>
+{
+	sentc_crypto_full::group::leave_group(base_url, auth_token.as_str(), jwt.as_str(), id.as_str()).await?;
+
+	Ok(())
+}
+
+//__________________________________________________________________________________________________
+//key rotation
+
+#[wasm_bindgen]
+pub fn prepare_key_rotation(pre_group_key: &str, public_key: &str) -> Result<String, JsValue>
+{
+	let out = group::key_rotation(pre_group_key, public_key)?;
+
+	Ok(out)
+}
+
+#[wasm_bindgen]
+pub fn done_key_rotation(private_key: &str, public_key: &str, pre_group_key: &str, server_output: &str) -> Result<String, JsValue>
+{
+	let out = group::done_key_rotation(private_key, public_key, pre_group_key, server_output)?;
+
+	Ok(out)
+}
+
+#[wasm_bindgen]
+pub async fn key_rotation(
+	base_url: String,
+	auth_token: String,
+	jwt: String,
+	id: String,
+	public_key: String,
+	pre_group_key: String,
+) -> Result<String, JsValue>
+{
+	let out = sentc_crypto_full::group::key_rotation(
+		base_url,
+		auth_token.as_str(),
+		jwt.as_str(),
+		id.as_str(),
+		public_key.as_str(),
+		pre_group_key.as_str(),
+	)
+	.await?;
+
+	Ok(out)
+}
+
+/**
+Get the keys for done key rotation.
+
+Then call for each key rotation server output the finish_key_rotation fn
+*/
+#[wasm_bindgen]
+pub async fn pre_done_key_rotation(base_url: String, auth_token: String, jwt: String, id: String) -> Result<JsValue, JsValue>
+{
+	let out = sentc_crypto_full::group::prepare_done_key_rotation(base_url, auth_token.as_str(), jwt.as_str(), id.as_str()).await?;
+
+	Ok(JsValue::from_serde(&out).unwrap())
+}
+
+#[wasm_bindgen]
+pub async fn finish_key_rotation(
+	base_url: String,
+	auth_token: String,
+	jwt: String,
+	id: String,
+	server_output: String,
+	pre_group_key: String,
+	public_key: String,
+	private_key: String,
+) -> Result<(), JsValue>
+{
+	sentc_crypto_full::group::done_key_rotation(
+		base_url,
+		auth_token.as_str(),
+		jwt.as_str(),
+		id.as_str(),
+		server_output.as_str(),
+		pre_group_key.as_str(),
+		public_key.as_str(),
+		private_key.as_str(),
+	)
+	.await?;
+
+	Ok(())
+}
+
+//__________________________________________________________________________________________________
