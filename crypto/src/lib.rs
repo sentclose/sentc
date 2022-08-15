81e7fed85c33d513e43f7538a849631d1ebd6d97
Jörn Heinemann <joernheinemann@gmx.de>
2022-08-15 12:02:32 +0200

    remove old test fn

--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -5,8 +5,6 @@ extern crate alloc;
 pub mod crypto;
 mod error;
 pub mod group;
-#[cfg(feature = "server")]
-pub mod test_fn;
 pub mod user;
 pub mod util;
 
@@ -18,4 +16,4 @@ because using common from path via submodule resolve in version conflicts when u
 pub use sentc_crypto_common as sdk_common;
 
 pub use self::error::{err_to_msg, SdkError};
-pub use self::util::{KeyData, PrivateKeyFormat, PublicKeyFormat, SignKeyFormat, SymKeyFormat, VerifyKeyFormat};
+pub use self::util::{KeyData, PrivateKeyFormat, PublicKeyFormat, SignKeyFormat, SymKeyFormat, UserData, VerifyKeyFormat};
