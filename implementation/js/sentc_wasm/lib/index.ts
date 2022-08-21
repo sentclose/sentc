a5089982f45550f120ebbe91d3df3315d1c24695
Jörn Heinemann <joernheinemann@gmx.de>
2022-08-21 22:11:33 +0200

    move ts out of wasm

--- implementation/js/sentc_wasm/lib/index.ts
+++
@@ -1,5 +0,0 @@
-export {Sentc, SentcOptions} from "./Sentc";
-export {StorageInterface} from "./core";
-export {SymKey} from "./crypto/SymKey";
-export {Group} from "./Group";
-export {User} from "./User";
