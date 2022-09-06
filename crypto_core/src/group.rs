ea2098a22728d65816e6e9f7864c85f87d212117
Jörn Heinemann <joernheinemann@gmx.de>
2022-09-06 17:19:13 +0200

    prepare user groups too for create and key rotation

--- crypto_core/src/group.rs
+++ crypto_core/src/group.rs
@@ -1,8 +1,9 @@
 use alloc::vec::Vec;
 
-use crate::alg::{asym, sym};
+use crate::alg::sym::aes_gcm::AesKey;
+use crate::alg::{asym, sign, sym};
 use crate::crypto::{decrypt_asymmetric, decrypt_symmetric, encrypt_asymmetric};
-use crate::{Error, Pk, Sk, SymKey};
+use crate::{AsymKeyOutput, Error, Pk, SignK, Sk, SymKey, VerifyK};
 
 pub struct CreateGroupOutput
 {
@@ -12,6 +13,11 @@ pub struct CreateGroupOutput
 	pub encrypted_private_group_key: Vec<u8>,
 	pub public_group_key: Pk,
 	pub keypair_encrypt_alg: &'static str,
+
+	//for user group
+	pub verify_key: Option<VerifyK>,
+	pub encrypted_sign_key: Option<Vec<u8>>,
+	pub keypair_sign_alg: Option<&'static str>,
 }
 
 pub struct KeyRotationOutput
@@ -25,6 +31,11 @@ pub struct KeyRotationOutput
 	pub encrypted_group_key_by_ephemeral: Vec<u8>,
 	pub ephemeral_alg: &'static str,
 	pub encrypted_ephemeral_key: Vec<u8>, //encrypted by the previous_group_key group key. encrypt this key with every other member public key on the server
+
+	//for user group
+	pub verify_key: Option<VerifyK>,
+	pub encrypted_sign_key: Option<Vec<u8>>,
+	pub keypair_sign_alg: Option<&'static str>,
 }
 
 pub struct PrepareGroupKeysForNewMemberOutput
@@ -35,7 +46,7 @@ pub struct PrepareGroupKeysForNewMemberOutput
 }
 
 #[cfg(feature = "argon2_aes_ecies_ed25519")]
-fn prepare_create_aes_ecies_ed25519(creators_public_key: &Pk) -> Result<CreateGroupOutput, Error>
+fn prepare_create_aes_ecies_ed25519(creators_public_key: &Pk, user_group: bool) -> Result<CreateGroupOutput, Error>
 {
 	//1. create the keys:
 	//	1. master symmetric key
@@ -49,20 +60,8 @@ fn prepare_create_aes_ecies_ed25519(creators_public_key: &Pk) -> Result<CreateGr
 		SymKey::Aes(k) => k,
 	};
 
-	let private_key = match &keypair.sk {
-		Sk::Ecies(k) => k,
-	};
-
-	let encrypted_private_group_key = sym::aes_gcm::encrypt_with_generated_key(raw_group_key, private_key)?;
-
-	//3. encrypt the group key with the creators public key. every other member will get this key encrypted by his/hers public key
-	let (encrypted_group_key, encrypted_group_key_alg) = match creators_public_key {
-		Pk::Ecies(_) => {
-			let en = asym::ecies::encrypt(creators_public_key, raw_group_key)?;
-
-			(en, asym::ecies::ECIES_OUTPUT)
-		},
-	};
+	let (encrypted_private_group_key, encrypted_group_key, encrypted_group_key_alg, verify_key, encrypted_sign_key, keypair_sign_alg) =
+		prepare_keys_aes_ecies_ed25519(creators_public_key, user_group, raw_group_key, &keypair)?;
 
 	Ok(CreateGroupOutput {
 		encrypted_group_key,
@@ -71,17 +70,20 @@ fn prepare_create_aes_ecies_ed25519(creators_public_key: &Pk) -> Result<CreateGr
 		group_key_alg: group_key.alg,
 		keypair_encrypt_alg: keypair.alg,
 		encrypted_group_key_alg,
+		verify_key,
+		encrypted_sign_key,
+		keypair_sign_alg,
 	})
 }
 
-pub fn prepare_create(creators_public_key: &Pk) -> Result<CreateGroupOutput, Error>
+pub fn prepare_create(creators_public_key: &Pk, user_group: bool) -> Result<CreateGroupOutput, Error>
 {
 	#[cfg(feature = "argon2_aes_ecies_ed25519")]
-	prepare_create_aes_ecies_ed25519(creators_public_key)
+	prepare_create_aes_ecies_ed25519(creators_public_key, user_group)
 }
 
 #[cfg(feature = "argon2_aes_ecies_ed25519")]
-fn key_rotation_aes_ecies_ed25519(previous_group_key: &SymKey, invoker_public_key: &Pk) -> Result<KeyRotationOutput, Error>
+fn key_rotation_aes_ecies_ed25519(previous_group_key: &SymKey, invoker_public_key: &Pk, user_group: bool) -> Result<KeyRotationOutput, Error>
 {
 	//1. create new group keys
 	let group_key = sym::aes_gcm::generate_key()?;
@@ -92,11 +94,8 @@ fn key_rotation_aes_ecies_ed25519(previous_group_key: &SymKey, invoker_public_ke
 		SymKey::Aes(k) => k,
 	};
 
-	let private_key = match &keypair.sk {
-		Sk::Ecies(k) => k,
-	};
-
-	let encrypted_private_group_key = sym::aes_gcm::encrypt_with_generated_key(raw_group_key, private_key)?;
+	let (encrypted_private_group_key, encrypted_group_key_by_user, encrypted_group_key_alg, verify_key, encrypted_sign_key, keypair_sign_alg) =
+		prepare_keys_aes_ecies_ed25519(invoker_public_key, user_group, raw_group_key, &keypair)?;
 
 	//3. create an ephemeral key to encrypt the new group key
 	let ephemeral_key = sym::aes_gcm::generate_key()?;
@@ -112,15 +111,6 @@ fn key_rotation_aes_ecies_ed25519(previous_group_key: &SymKey, invoker_public_ke
 		SymKey::Aes(k) => sym::aes_gcm::encrypt_with_generated_key(k, raw_ephemeral_key)?,
 	};
 
-	//6. encrypt the new group key with the public key of the invoker, so we save one encryption on the server
-	let (encrypted_group_key_by_user, encrypted_group_key_alg) = match invoker_public_key {
-		Pk::Ecies(_) => {
-			let en = asym::ecies::encrypt(invoker_public_key, raw_group_key)?;
-
-			(en, asym::ecies::ECIES_OUTPUT)
-		},
-	};
-
 	Ok(KeyRotationOutput {
 		encrypted_group_key_by_user,
 		encrypted_group_key_alg,
@@ -130,14 +120,73 @@ fn key_rotation_aes_ecies_ed25519(previous_group_key: &SymKey, invoker_public_ke
 		encrypted_group_key_by_ephemeral,
 		public_group_key: keypair.pk,
 		encrypted_ephemeral_key,
+		verify_key,
+		encrypted_sign_key,
+		keypair_sign_alg,
 		ephemeral_alg: sym::aes_gcm::AES_GCM_OUTPUT,
 	})
 }
 
-pub fn key_rotation(previous_group_key: &SymKey, invoker_public_key: &Pk) -> Result<KeyRotationOutput, Error>
+pub fn key_rotation(previous_group_key: &SymKey, invoker_public_key: &Pk, user_group: bool) -> Result<KeyRotationOutput, Error>
 {
 	#[cfg(feature = "argon2_aes_ecies_ed25519")]
-	key_rotation_aes_ecies_ed25519(previous_group_key, invoker_public_key)
+	key_rotation_aes_ecies_ed25519(previous_group_key, invoker_public_key, user_group)
+}
+
+#[cfg(feature = "argon2_aes_ecies_ed25519")]
+fn prepare_keys_aes_ecies_ed25519(
+	public_key: &Pk,
+	user_group: bool,
+	raw_group_key: &AesKey,
+	key_pair: &AsymKeyOutput,
+) -> Result<
+	(
+		Vec<u8>,
+		Vec<u8>,
+		&'static str,
+		Option<VerifyK>,
+		Option<Vec<u8>>,
+		Option<&'static str>,
+	),
+	Error,
+>
+{
+	let private_key = match &key_pair.sk {
+		Sk::Ecies(k) => k,
+	};
+
+	let encrypted_private_group_key = sym::aes_gcm::encrypt_with_generated_key(raw_group_key, private_key)?;
+
+	let (encrypted_group_key, encrypted_group_key_alg) = match public_key {
+		Pk::Ecies(_) => {
+			let en = asym::ecies::encrypt(public_key, raw_group_key)?;
+
+			(en, asym::ecies::ECIES_OUTPUT)
+		},
+	};
+
+	let (verify_key, encrypted_sign_key, keypair_sign_alg) = if !user_group {
+		(None, None, None)
+	} else {
+		let sign = sign::ed25519::generate_key_pair()?;
+
+		let sign_key = match &sign.sign_key {
+			SignK::Ed25519(k) => k,
+		};
+
+		let encrypted_sign_key = sym::aes_gcm::encrypt_with_generated_key(raw_group_key, sign_key)?;
+
+		(Some(sign.verify_key), Some(encrypted_sign_key), Some(sign.alg))
+	};
+
+	Ok((
+		encrypted_private_group_key,
+		encrypted_group_key,
+		encrypted_group_key_alg,
+		verify_key,
+		encrypted_sign_key,
+		keypair_sign_alg,
+	))
 }
 
 pub fn done_key_rotation(
@@ -321,7 +370,7 @@ mod test
 	{
 		let (pk, login_out) = create_dummy_user();
 
-		let group_out = prepare_create(&pk).unwrap();
+		let group_out = prepare_create(&pk, false).unwrap();
 
 		#[cfg(feature = "argon2_aes_ecies_ed25519")]
 		assert_eq!(group_out.group_key_alg, AES_GCM_OUTPUT);
@@ -361,7 +410,7 @@ mod test
 	{
 		let (pk, login_out) = create_dummy_user();
 
-		let group_out = prepare_create(&pk).unwrap();
+		let group_out = prepare_create(&pk, false).unwrap();
 
 		#[cfg(feature = "argon2_aes_ecies_ed25519")]
 		assert_eq!(group_out.group_key_alg, AES_GCM_OUTPUT);
@@ -376,7 +425,7 @@ mod test
 		)
 		.unwrap();
 
-		let rotation_out = key_rotation(&group_key, &pk).unwrap();
+		let rotation_out = key_rotation(&group_key, &pk, false).unwrap();
 
 		//it should get the values from own encrypted group key
 		let (new_group_key, _new_group_pri_key) = get_group(
@@ -437,7 +486,7 @@ mod test
 		let (user_1_pk, user_1_out) = create_dummy_user();
 		let (user_2_pk, user_2_out) = create_dummy_user();
 
-		let group_out = prepare_create(&user_1_pk).unwrap();
+		let group_out = prepare_create(&user_1_pk, false).unwrap();
 		let (group_key, _group_pri_key) = get_group(
 			&user_1_out.private_key,
 			&group_out.encrypted_group_key,
@@ -448,7 +497,7 @@ mod test
 		.unwrap();
 
 		//create multiple group keys
-		let rotation_out = key_rotation(&group_key, &user_1_pk).unwrap();
+		let rotation_out = key_rotation(&group_key, &user_1_pk, false).unwrap();
 		let (new_group_key, _new_group_pri_key) = get_group(
 			&user_1_out.private_key,
 			&rotation_out.encrypted_group_key_by_user,
@@ -458,7 +507,7 @@ mod test
 		)
 		.unwrap();
 
-		let rotation_out_1 = key_rotation(&new_group_key, &user_1_pk).unwrap();
+		let rotation_out_1 = key_rotation(&new_group_key, &user_1_pk, false).unwrap();
 		let (new_group_key_1, _new_group_pri_key_1) = get_group(
 			&user_1_out.private_key,
 			&rotation_out_1.encrypted_group_key_by_user,
@@ -468,7 +517,7 @@ mod test
 		)
 		.unwrap();
 
-		let rotation_out_2 = key_rotation(&new_group_key_1, &user_1_pk).unwrap();
+		let rotation_out_2 = key_rotation(&new_group_key_1, &user_1_pk, false).unwrap();
 		let (new_group_key_2, _new_group_pri_key_2) = get_group(
 			&user_1_out.private_key,
 			&rotation_out_2.encrypted_group_key_by_user,
