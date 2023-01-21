410a41fc6bb3031b371308d18d8a0927e1b88007
Jörn Heinemann <joernheinemann@gmx.de>
2023-01-22 00:12:52 +0100

    add search encrypted item with req

--- crypto_full/src/crypto/non_rust.rs
+++ crypto_full/src/crypto/non_rust.rs
@@ -1,9 +1,11 @@
 use alloc::string::String;
 use alloc::vec::Vec;
 
+use sentc_crypto_common::content_searchable::ListSearchItem;
 use sentc_crypto_common::SymKeyId;
 
 pub type GenKeyRes = Result<(String, String), String>;
 pub type KeyRes = Result<String, String>;
 pub type KeysRes = Result<(Vec<String>, u128, SymKeyId), String>;
 pub type VoidRes = Result<(), String>;
+pub type SearchRes = Result<Vec<ListSearchItem>, String>;
