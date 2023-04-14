a174ed65cab2367c5238efdfe4811b4db1ba41a0
Jörn Heinemann <joernheinemann@gmx.de>
2023-04-15 00:15:22 +0200

    impl the new file for crypto full

--- crypto_full/src/file/non_rust.rs
+++ crypto_full/src/file/non_rust.rs
@@ -5,7 +5,8 @@ use sentc_crypto_common::file::{FileData, FilePartListItem};
 
 pub type FileRes = Result<FileData, String>;
 pub type FilePartRes = Result<Vec<FilePartListItem>, String>;
-pub type ByteRes = Result<Vec<u8>, String>;
+pub type ByteRes = Result<(Vec<u8>, String), String>;
 pub type VoidRes = Result<(), String>;
+pub type KeyRes = Result<String, String>;
 
 pub type FileRegRes = Result<(String, String, String), String>;
--- crypto_full/src/file/rust.rs
+++ crypto_full/src/file/rust.rs
@@ -1,13 +1,15 @@
 use alloc::string::String;
 use alloc::vec::Vec;
 
+use sentc_crypto::sdk_core::SymKey;
 use sentc_crypto_common::file::{FileData, FilePartListItem};
 
 use crate::error::SdkFullError;
 
 pub type FileRes = Result<FileData, SdkFullError>;
 pub type FilePartRes = Result<Vec<FilePartListItem>, SdkFullError>;
-pub type ByteRes = Result<Vec<u8>, SdkFullError>;
+pub type ByteRes = Result<(Vec<u8>, SymKey), SdkFullError>;
 pub type VoidRes = Result<(), SdkFullError>;
+pub type KeyRes = Result<String, SdkFullError>;
 
 pub type FileRegRes = Result<(String, String, Option<String>), SdkFullError>;
