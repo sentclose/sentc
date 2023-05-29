b3eead4c5d4975fa2d5a9b14b544f6779cd4689e
Jörn Heinemann <joernheinemann@gmx.de>
2023-05-29 17:00:35 +0200

    return also the sign key id of a user public key to get the right verify key when verify the user public key

--- crypto_full/src/user/non_rust.rs
+++ crypto_full/src/user/non_rust.rs
@@ -3,14 +3,14 @@ use alloc::vec::Vec;
 
 use sentc_crypto::{UserData, UserKeyData};
 use sentc_crypto_common::user::{UserDeviceList, UserInitServerOutput};
-use sentc_crypto_common::EncryptionKeyPairId;
+use sentc_crypto_common::{EncryptionKeyPairId, SignKeyPairId};
 
 pub type Res = Result<String, String>;
 pub type VoidRes = Result<(), String>;
 pub type BoolRes = Result<bool, String>;
 pub type LoginRes = Result<UserData, String>;
 pub type UserKeyFetchRes = Result<UserKeyData, String>;
-pub type UserPublicKeyRes = Result<(String, EncryptionKeyPairId), String>;
+pub type UserPublicKeyRes = Result<(String, EncryptionKeyPairId, Option<SignKeyPairId>), String>;
 pub type UserVerifyKeyRes = Result<String, String>;
 pub type InitRes = Result<UserInitServerOutput, String>;
 pub type SessionRes = Result<(Option<String>, String), String>;
