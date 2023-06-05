8a3d4a128863fb6f3f89002db93d9a8368f40220
Jörn Heinemann <joernheinemann@gmx.de>
2023-06-05 23:26:26 +0200

    use new entities for sdk full crypto mod

--- crypto_full/src/crypto/rust.rs
+++ crypto_full/src/crypto/rust.rs
@@ -1,14 +1,14 @@
 use alloc::string::String;
 use alloc::vec::Vec;
 
-use sentc_crypto::SymKeyFormat;
+use sentc_crypto::entities::keys::SymKeyFormatInt;
 use sentc_crypto_common::content_searchable::ListSearchItem;
 use sentc_crypto_common::SymKeyId;
 
 use crate::error::SdkFullError;
 
-pub type GenKeyRes = Result<(String, SymKeyFormat), SdkFullError>;
-pub type KeyRes = Result<SymKeyFormat, SdkFullError>;
-pub type KeysRes = Result<(Vec<SymKeyFormat>, u128, SymKeyId), SdkFullError>;
+pub type GenKeyRes = Result<(String, SymKeyFormatInt), SdkFullError>;
+pub type KeyRes = Result<SymKeyFormatInt, SdkFullError>;
+pub type KeysRes = Result<(Vec<SymKeyFormatInt>, u128, SymKeyId), SdkFullError>;
 pub type VoidRes = Result<(), SdkFullError>;
 pub type SearchRes = Result<Vec<ListSearchItem>, SdkFullError>;
