376c3ccece474c9db80af9ec3a4df0ec9cae0b95
Jörn Heinemann <joernheinemann@gmx.de>
2023-10-07 09:57:40 +0200

    fixed clippy warning

--- crypto/src/group/group_rank_check.rs
+++ crypto/src/group/group_rank_check.rs
@@ -9,6 +9,7 @@ pub type VoidRes = Result<(), SdkError>;
 #[cfg(not(feature = "rust"))]
 pub type VoidRes = Result<(), String>;
 
+#[allow(clippy::needless_return_with_question_mark)]
 pub fn check_kick_user(user_rank: i32, admin_rank: i32) -> VoidRes
 {
 	if admin_rank > 2 {
@@ -23,6 +24,7 @@ pub fn check_kick_user(user_rank: i32, admin_rank: i32) -> VoidRes
 	Ok(())
 }
 
+#[allow(clippy::needless_return_with_question_mark)]
 pub fn check_group_delete(admin_rank: i32) -> VoidRes
 {
 	if admin_rank > 1 {
@@ -32,6 +34,7 @@ pub fn check_group_delete(admin_rank: i32) -> VoidRes
 	Ok(())
 }
 
+#[allow(clippy::needless_return_with_question_mark)]
 pub fn check_delete_user_rank(admin_rank: i32) -> VoidRes
 {
 	if admin_rank > 2 {
@@ -41,6 +44,7 @@ pub fn check_delete_user_rank(admin_rank: i32) -> VoidRes
 	Ok(())
 }
 
+#[allow(clippy::needless_return_with_question_mark)]
 pub fn check_get_join_reqs(admin_rank: i32) -> VoidRes
 {
 	if admin_rank > 2 {
@@ -50,6 +54,7 @@ pub fn check_get_join_reqs(admin_rank: i32) -> VoidRes
 	Ok(())
 }
 
+#[allow(clippy::needless_return_with_question_mark)]
 pub fn check_make_invite_req(admin_rank: i32) -> VoidRes
 {
 	if admin_rank > 2 {
@@ -59,6 +64,7 @@ pub fn check_make_invite_req(admin_rank: i32) -> VoidRes
 	Ok(())
 }
 
+#[allow(clippy::needless_return_with_question_mark)]
 pub fn check_create_sub_group(admin_rank: i32) -> VoidRes
 {
 	if admin_rank > 1 {
@@ -68,6 +74,7 @@ pub fn check_create_sub_group(admin_rank: i32) -> VoidRes
 	Ok(())
 }
 
+#[allow(clippy::needless_return_with_question_mark)]
 pub fn check_sent_join_req_list(admin_rank: i32) -> VoidRes
 {
 	if admin_rank > 1 {
