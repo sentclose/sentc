2d0762e0f07fc69e37f16e2f8fd977384c52cd6d
Jörn Heinemann <joernheinemann@gmx.de>
2024-02-03 00:20:20 +0100

    fixed wrong error type in rust impl

--- crypto_light_full/src/user/rust.rs
+++ crypto_light_full/src/user/rust.rs
@@ -26,6 +26,6 @@ pub type LoginRes = Result<UserDataInt, SdkLightError>;
 pub type InitRes = Result<UserInitServerOutput, SdkLightError>;
 pub type DeviceListRes = Result<Vec<UserDeviceList>, SdkLightError>;
 
-pub type RegisterRawOtpRes = Result<OtpRegister, String>;
-pub type RegisterOtpRes = Result<(String, Vec<String>), String>;
-pub type OtpRecoveryKeyRes = Result<OtpRecoveryKeysOutput, String>;
+pub type RegisterRawOtpRes = Result<OtpRegister, SdkLightError>;
+pub type RegisterOtpRes = Result<(String, Vec<String>), SdkLightError>;
+pub type OtpRecoveryKeyRes = Result<OtpRecoveryKeysOutput, SdkLightError>;
